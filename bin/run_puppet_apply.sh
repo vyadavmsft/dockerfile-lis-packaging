579a3c96f88f165112995265214f5cefbae0b3f7
peter j. pouliot <ppouliot@gmail.com>
2017-03-22 20:35:05 -0700

    fixing typo

--- bin/run_puppet_apply.sh
+++ bin/run_puppet_apply.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-puppet apply --debug --trace --verbose --modulepath=/etc/puppetlabs/code/environments/production/modules /etc/puppet/code/production/manifests
+puppet apply --debug --trace --verbose --modulepath=/etc/puppetlabs/code/environments/production/modules /etc/puppetlabs/code/production/manifests
