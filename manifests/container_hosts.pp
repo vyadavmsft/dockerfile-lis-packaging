836c14b3c64fa5d873fba27fbe56568b6a16b4d7
peter j. pouliot <ppouliot@gmail.com>
2017-03-22 20:27:29 -0700

    adding node def for container hosts

---
+++ manifests/container_hosts.pp
@@ -0,0 +1,53 @@
+node 'docker-centos','docker-ubuntu' {
+  Docker::Run{ ensure => 'absent', }
+  class {'docker':
+    tcp_bind    => 'tcp://0.0.0.0:4243',
+    socket_bind => 'unix:///var/run/docker.sock',
+  }
+  vcsrepo{'/usr/local/src/dockerfile-sentinel-all':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/openstack-hyper-v/dockerfile-sentinel-all.git'
+  }
+
+
+  case $operatingsystem {
+    'Ubuntu':{
+      notice('This ubuntu container host executes Ubuntu Based container jobs')
+      docker::image{'msopenstack/sentinel-debian':
+        ensure  =>  'latest',
+      }
+      docker::image{'msopenstack/sentinel-ubuntu_xenial':
+        ensure  =>  'latest',
+      }
+      docker::image{'msopenstack/sentinel-ubuntu_trusty':
+        ensure  =>  'latest',
+      }
+    }
+    'Centos':{
+      notice('This centos container hosts runs a local postgresql server, operations production puppet infra containers, and executes centos based contianer jobs.')
+
+      docker::image{'centos':
+        image_tag =>  ['centos7'],
+        ensure  =>  'latest',
+      }
+
+      docker::image{'msopenstack/sentinel-centos':
+        ensure  =>  'latest',
+      }
+      docker::image{'msopenstack/sentinel-fedora':
+        ensure  =>  'latest',
+      }
+      docker::image{'msopenstack/sentinel-oraclelinux':
+        ensure  =>  'latest',
+      }
+
+     }
+
+    }
+    default:{
+      warning("Unsupported ${operatingsystem}")
+    }
+  }
+
+}
